a8a02a2c350c00ce3951015d597a0ee61acca500
soraj92 <34873291+soraj92@users.noreply.github.com>
2018-08-20 16:33:03 +0900

    수정

---
+++ test.js
@@ -0,0 +1,14 @@
+const myAge = 27;
+let earlyYears=2;
+earlyYears*=10.5;
+let laterYears=myAge-2;
+laterYears*=4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+let myAgeInDogYears = earlyYears+laterYears;
+
+const myName="sora".toLowerCase();
+
+console.log(`my name is ${myName}. i am ${myAgeInDogYears} old in dog years`);
